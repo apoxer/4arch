fa2b3e0603c0a011c2ca1895c53515b1ee6fae8a
apoxer <allenallen7731@gmail.com>
2025-01-17 15:55:31 +0530

    updte whole sht frm scrpt

--- scripts/3gatherconfigs.sh
+++ scripts/3gatherconfigs.sh
@@ -1,18 +1,26 @@
-echo GATHERING_CONFIGSSSSSSSS..................................................
+echo GATHERING_CONFIGSSSSSSSS.................................................
 
-rm -rf ~/carchy/confs
-mkdir ~/carchy/confs
+rm -rf ~/4arch/confs
+mkdir ~/4arch/confs
 
-cp ~/.config/fish/config.fish ~/carchy/confs
-cp ~/.config/uwsm/env ~/carchy/confs
+cp ~/.config/fish/config.fish ~/4arch/confs
+cp ~/.config/uwsm/env ~/4arch/confs
 
-cp ~/.config/kitty/kitty.conf ~/carchy/confs
-cp ~/.config/kitty/Catppuccin-Mocha.conf ~/carchy/confs
+cp ~/.config/kitty/kitty.conf ~/4arch/confs
+cp ~/.config/kitty/Catppuccin-Mocha.conf ~/4arch/confs
 
-cp ~/.config/fuzzel/fuzzel.ini ~/carchy/confs
+cp ~/.config/fuzzel/fuzzel.ini ~/4arch/confs
 
-cp ~/.config/hypr/hyprland.conf ~/carchy/confs
-cp ~/.config/hypr/hypridle.conf ~/carchy/confs
-cp ~/.config/hypr/hyprlock.conf ~/carchy/confs
+cp ~/.config/hypr/hyprland.conf ~/4arch/confs
+cp ~/.config/hypr/hypridle.conf ~/4arch/confs
+cp ~/.config/hypr/hyprlock.conf ~/4arch/confs
 
-echo DONE_MWONEY...............................................................
+echo GIT_SHIT.................................................................
+cd
+cd ~/4arch
+git add .
+git commit -m "updte whole sht frm scrpt"
+git push origin main
+cd
+
+echo DONE_MWONEY..............................................................
